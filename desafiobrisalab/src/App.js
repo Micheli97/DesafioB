a34487d1d56a2687ad0f2f96bbde1825fef92d63
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-01 20:40:18 -0300

    Inicial

---
+++ desafiobrisalab/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Login from "./components/login/Login";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login></Login>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ desafiobrisalab/src/components/login/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Logo from "./"
+
+import "./Login.css";
+
+export default (props) => {
+  return (
+    <div>
+        <div >
+            <img src = "image1.png" ></img>
+        </div>
+      <div>
+        <h1 className="title">Comece a coletar pokemons</h1>
+      </div>
+    </div>
+  );
+};
