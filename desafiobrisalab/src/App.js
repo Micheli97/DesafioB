3d7e8120fa815cf34452e8f1bbfcea3b2fa90c48
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-02 09:35:12 -0300

    Login - #Ajustes

--- desafiobrisalab/src/App.js
+++ desafiobrisalab/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Home from "./components/home/Home";
-import Login from './components/login/Login';
+import Login from "./components/login/Login"
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home></Home>
-        {/* <Login></Login> */}
+        {/* <Home></Home> */}
+        <Login></Login>
        
        
       </header>
