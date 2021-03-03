2b89f2a8df47d795db42706fdf440eb5864cafd1
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-03 16:45:34 -0300

    Login - concluded

--- desafiobrisalab/src/components/login/Login.js
+++ desafiobrisalab/src/components/login/Login.js
@@ -7,13 +7,12 @@ import "./Login.css";
 function LoginScreen() {
   return (
     <div className="background">
-      {/* Falta arrumar a imagem de background */}
-
+      
       <div className="form">
         <div className="logoLogin"></div>
         <h2>Comece a coletar pokémons!</h2>
         <Forms></Forms>
-        <LoginButton></LoginButton>
+       
       </div>
     </div>
   );
