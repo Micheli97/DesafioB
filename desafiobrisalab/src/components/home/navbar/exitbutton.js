06e24a57f78ce84d81d437d064d377545402fe27
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-02 22:31:32 -0300

    Navbar

---
+++ desafiobrisalab/src/components/home/navbar/exitbutton.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./navbar.css";
+
+class ExitButton extends React.Component{
+    render (){
+        return <button className="button">Sair</button>
+    }
+}
+export default ExitButton
--- desafiobrisalab/src/components/home/navbar/navbar.js
+++ desafiobrisalab/src/components/home/navbar/navbar.js
@@ -1,19 +1,28 @@
 import React from "react";
+import ExitButton from "./exitbutton";
 import "./navbar.css";
-function Navbar(){
+
+function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          <a>Favorito</a>
-        </li>
-        <li>
-          <a>Procurar</a>
-        </li>
-        <li>
-          <a>Ver todos</a>
-        </li>
-      </ul>
+      <div className="menu">
+        <div className="logo"></div>
+        <ul>
+          <li>
+            <a>Favorito</a>
+          </li>
+          <li>
+            <a>Procurar</a>
+          </li>
+          <li>
+            <a>Ver todos</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <button className="button">Sair</button>
+      </div>
     </nav>
   );
 }
