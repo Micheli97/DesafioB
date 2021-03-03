74ec3f5802a7be6e97cfdb97d445fe746a0f6de8
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-03 17:30:11 -0300

    Routes #1

--- desafiobrisalab/src/App.js
+++ desafiobrisalab/src/App.js
@@ -1,7 +1,27 @@
-import './App.css';
-import Routes from "./routes/routes"
+import React from "react";
+import Routes from "./components/routes/routes"
+import Login from "./components/login/Login";
+import Pokemonlist from "./components/home/pokemon-list/pokemon-list"
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
 
-const App = () => <Routes></Routes>
+function App() {
+  return (
+
+    <Routes />
+    // <BrowserRouter>
+    //   <switch>
+    //     <Route exact path="/" >
+    //       <Pokemonlist />
+    //     </Route>
+    //     <Route exact path="/pokemon-list" >
+    //       <Pokemonlist />
+    //     </Route>
+        
+    //   </switch>
+    // </BrowserRouter>
+  );
+}
 
 export default App;
