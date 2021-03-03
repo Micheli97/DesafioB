4c76edb820ce2937b265fd05216ec8aa4044b5de
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-02 23:20:06 -0300

    Search

---
+++ desafiobrisalab/src/components/home/search/search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../navbar/navbar";
+import './search.css';
+
+function Search() {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default Search
