0e59e626acbdf1ea8c945af1925b23cc0c2c2aff
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-03 01:37:47 -0300

    Favorite - body

--- desafiobrisalab/src/components/home/favorite/favorite.js
+++ desafiobrisalab/src/components/home/favorite/favorite.js
@@ -1,13 +1,15 @@
 import React from "react";
-import NavBar from "../navbar/navbar"
+import NavBar from "../navbar/navbar";
 import "./favorite.css";
 
 function Favorite() {
   return (
     <div className="body">
       <NavBar></NavBar>
-      <div className="astronaut">
-      </div>
+      <div className="astronaut"></div>
+      <h3>Está meio vazio por aqui!</h3>
+      <h5>Procure por pokémons para adicioná-los aos seus favoritos</h5>
+      <button className="searchPokemonsButton">Procurar pokémons</button>
     </div>
   );
 }
