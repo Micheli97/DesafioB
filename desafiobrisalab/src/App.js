5fd09df75b124a5347aaf73bbbc77644f4c71fe5
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-03 01:26:31 -0300

    Login - correção logo

--- desafiobrisalab/src/App.js
+++ desafiobrisalab/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-// import Login from "./components/login/Login"
+import Login from "./components/login/Login"
 import Favorite from "./components/home/favorite/favorite"
 import Search from "./components/home/search/search"
+import Listining from "./components/home/listing/listining"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <Home></Home> */}
-        {/* <Login></Login> */}
+        <Login></Login>
         {/* <Favorite></Favorite> */}
-        <Search></Search>
+        {/* <Search></Search> */}
+        {/* <Listining></Listining> */}
        
        
       </header>
--- desafiobrisalab/src/components/login/Login.js
+++ desafiobrisalab/src/components/login/Login.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Forms from "./Foms";
 import LoginButton from "./LoginButton";
+
 import "./Login.css";
 
 function LoginScreen() {
   return (
     <div className="background">
       {/* Falta arrumar a imagem de background */}
-      
+
       <div className="form">
-        <div className="logo"></div>
+        <div className="logoLogin"></div>
         <h2>Comece a coletar pokémons!</h2>
         <Forms></Forms>
         <LoginButton></LoginButton>
