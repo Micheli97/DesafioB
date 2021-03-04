6bffd2763a4268b4be34db182e7554f8fb7ddb9d
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-03-04 01:39:51 -0300

    Search-box

---
+++ desafiobrisalab/src/components/search/search-box.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./search.css";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+
+class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: undefined,
+    };
+  }
+
+  search(event) {
+    const search = event.target.search.value;
+    this.preventDefault();
+  }
+
+  renderSearchBox() {
+    return (
+      <div>
+        <form className="searchBox">
+          <InputBase placeholder="Pesquisar" />
+          <IconButton className="icon-search" type="submit">
+            <SearchIcon />
+          </IconButton>
+        </form>
+      </div>
+    );
+  }
+
+  render() {
+    return <div>{this.renderSearchBox()}</div>;
+  }
+}
+
+export default SearchBox;
---
+++ desafiobrisalab/src/components/search/search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navbar/navbar";
+import './search.css';
+import SearchBox from "./search-box"
+
+function Search() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <SearchBox></SearchBox>
+    </div>
+  );
+}
+
+export default Search
